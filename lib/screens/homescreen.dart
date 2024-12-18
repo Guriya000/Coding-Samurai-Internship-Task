858076cda94624733fe1a2b7a077ce04d296a2d4
Laraib <laraibazhar107@gmail.com>
2024-12-18 18:31:28 +0500

    change homescreen's background

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -12,7 +12,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.black,
       extendBodyBehindAppBar: true,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
